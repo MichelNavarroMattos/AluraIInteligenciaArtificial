e6b81f3f2f0c4f30edbcc5c5eff2919e8487f35e
MichelNavarroMattos <148919974+MichelNavarroMattos@users.noreply.github.com>
2024-09-07 00:11:03 -0300

    Add files via upload

---
+++ AluraDados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "inteligencia artificial",
+        descricao: "A inteligência artificial (IA) deixou de ser ficção científica para se tornar.uma realidade presente em nosso dia a dia. Seus avanços são exponenciais, permeando diversos setores, desde a saúde, com diagnósticos mais precisos, até a indústria, com a automatização de processos.",
+        link: "https://www.microsoft.com/pt-br/ai"
+    },
+    {
+        titulo: "Aprendizado de máquina",
+        descricao: "O aprendizado de máquina é um subcampo da inteligência artificial que permite que sistemas aprendam com dados, sem serem explicitamente programados.",
+        link: "https://www.ibm.com/br-pt/topics/machine-learning"
+    },
+    {
+        titulo: "Deep learning",
+        descricao: "O deep learning é um subcampo do aprendizado de máquina que utiliza redes neurais artificiais para modelar abstrações de alto nível em dados.",
+        link: "https://www.tensorflow.org/about/developers"
+    },
+];
+
